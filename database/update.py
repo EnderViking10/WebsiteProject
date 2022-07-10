06caa6e2b578cc23f80ea410e24cec1012c84e82
Erik Klem <mrerikk6@gmail.com>
2022-07-09 22:16:15 -0500

    Added create_user function
    Added update_user function
    
    Moved delete_item function to end of file

--- database/update.py
+++ database/update.py
@@ -1,6 +1,8 @@
+from werkzeug.security import generate_password_hash
+
 from . import get_item
 from ._get_db import get_db
-from ..forms import AdminUserForm, CreateItemForm, UpdateItemForm
+from ..forms import AdminUserForm, CreateItemForm, UpdateItemForm, RegisterForm
 
 
 def create_item(form: CreateItemForm) -> None:
@@ -11,6 +13,18 @@ def create_item(form: CreateItemForm) -> None:
         ' (name, description, cost)'
         ' VALUES (?, ?, ?)',
         (form.name.data, form.description.data, form.cost.data)
+    )
+    db.commit()  # Commits the changes
+
+
+def create_user(form: RegisterForm) -> None:
+    db = get_db()  # Gets the database
+    # Updates the database
+    db.execute(
+        'INSERT INTO user'
+        ' (username, password, user_level)'
+        ' VALUES (?, ?, ?)',
+        (form.username.data, generate_password_hash(form.password.data), 0)
     )
     db.commit()  # Commits the changes
 
@@ -28,11 +42,17 @@ def update_item(item_id: int, form: UpdateItemForm) -> None:
     db.commit()  # Commits the changes
 
 
-def delete_item(item_id: int) -> None:
-    get_item(item_id)
-    db = get_db()
-    db.execute('DELETE FROM items WHERE id = ?', (item_id,))
-    db.commit()
+def update_user(user_id: int, password: str) -> None:
+    db = get_db()  # Gets the database
+    # Updates the item in the database
+    db.execute(
+        'UPDATE user'
+        ' SET (password)'
+        ' = ?'
+        ' WHERE id = ?',
+        (password, user_id)
+    )
+    db.commit()  # Commits the changes
 
 
 def admin_update_user(user_id: int, form: AdminUserForm) -> None:
@@ -46,3 +66,10 @@ def admin_update_user(user_id: int, form: AdminUserForm) -> None:
         (form.user_level.data, user_id)
     )
     db.commit()  # Commits the changes
+
+
+def delete_item(item_id: int) -> None:
+    get_item(item_id)
+    db = get_db()
+    db.execute('DELETE FROM items WHERE id = ?', (item_id,))
+    db.commit()
