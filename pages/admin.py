9cbe6b8066d9a4a18e01fb75362fce9e488e754f
Erik Klem <mrerikk6@gmail.com>
2022-07-09 22:08:22 -0500

    Changed AdminUserForm to AdminPageForm
    Changed user_page.html to admin_user.html
    
    Added user level checking

--- pages/admin.py
+++ pages/admin.py
@@ -1,11 +1,11 @@
 import functools
 
 from flask import (
-    Blueprint, redirect, render_template, request, url_for, g
+    Blueprint, redirect, render_template, request, url_for, g, flash
 )
 
 from ..database import get_user_by_id, get_user_by_name, admin_update_user
-from ..forms import AdminSearchForm, AdminUserForm
+from ..forms import AdminPageForm, AdminUserForm
 
 bp = Blueprint('admin', __name__)
 
@@ -24,7 +24,7 @@ def admin_required(view):
 @bp.route('/admin', methods=('GET', 'POST'))
 @admin_required
 def admin():
-    form = AdminSearchForm()
+    form = AdminPageForm()
     if request.method == 'POST':
         user = get_user_by_name(form.username.data)
         user_id = user['id']
@@ -39,9 +39,16 @@ def admin():
 def user_page(user_id):
     user = get_user_by_id(user_id)
     form = AdminUserForm()
+    error = None
 
     if request.method == 'POST':
-        admin_update_user(user_id, form)
-        return redirect(url_for('admin.user_page', user_id=user_id))
+        if g.user['user_level'] < int(form.user_level.data):
+            error = f"You are not authorized to do that"
 
-    return render_template('admin/user_page.html', user=user, form=form)
+        if error is None:
+            admin_update_user(user_id, form)
+            return redirect(url_for('admin.user_page', user_id=user_id))
+
+        flash(error)
+
+    return render_template('admin/admin_user.html', user=user, form=form)
