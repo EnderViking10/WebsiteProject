42fc67dac72763cfcc66120fb40a6cb3977821a2
Erik Klem <mrerikk6@gmail.com>
2022-07-09 22:03:53 -0500

    Added update_user and create_user function

--- database/__init__.py
+++ database/__init__.py
@@ -2,4 +2,4 @@ from ._close_db import close_db
 from ._get_db import get_db
 from ._init_db_command import init_app
 from .get import get_all_items, get_item, get_user_by_id, get_user_by_name
-from .update import update_item, delete_item, create_item, admin_update_user
+from .update import update_item, delete_item, create_item, admin_update_user, update_user, create_user
