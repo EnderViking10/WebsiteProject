748a78eaf571f9bd72113830337c035598da3d8e
Erik Klem <mrerikk6@gmail.com>
2022-07-10 13:36:51 -0500

    Added get_all_tickets, get_all_users, delete_ticket, create_ticket functions

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,5 @@
 from ._close_db import close_db
 from ._get_db import get_db
 from ._init_db_command import init_app
-from .get import get_all_items, get_item, get_user_by_id, get_user_by_name
-from .update import update_item, delete_item, create_item, admin_update_user, update_user, create_user
+from .get import get_all_items, get_item, get_user_by_id, get_user_by_name, get_all_tickets, get_all_users
+from .update import update_item, delete_item, create_item, admin_update_user, update_user, create_user, delete_ticket, create_ticket
